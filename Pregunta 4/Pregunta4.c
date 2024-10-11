5e0cd5ab94d414f08d18023c7fa58dec2f7748e3
DanielelD <111719356+DanielelD@users.noreply.github.com>
2024-10-10 23:32:05 -0400

    Add files via upload

--- Pregunta 4/Pregunta4.c
+++ Pregunta 4/Pregunta4.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+void calcular_pi_iterativo(int *n, double *pi) {
+    int i;
+    double signo = 1.0;
+    *pi = 0.0;
+    
+    for(i = 0; i < *n; i++) {
+        *pi += signo / (2.0 * i + 1.0);
+        signo = -signo;  
+    }
+    
+    *pi *= 4.0;  
+}
+
+int main() {
+    int iteraciones;
+    double pi;
+    
+    printf("Ingrese el número de iteraciones: ");
+    scanf("%d", &iteraciones);
+    
+    calcular_pi_iterativo(&iteraciones, &pi);
+    
+    printf("El valor aproximado de pi (iterativo) es: %.15f\n", pi);
+    
+    return 0;
+}
