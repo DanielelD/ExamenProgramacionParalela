271d0c8af9d83b777635d8ee775caf5e10bc01ed
DanielelD <111719356+DanielelD@users.noreply.github.com>
2024-10-10 23:35:58 -0400

    Add files via upload

--- Pregunta 6/Pregunta6.c
+++ Pregunta 6/Pregunta6.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+int fibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    int fib[2] = {0, 1}; 
+    int siguiente;
+    
+    for (int i = 2; i <= n; i++) {
+        siguiente = fib[0] + fib[1];  
+        fib[0] = fib[1];  
+        fib[1] = siguiente; 
+    }
+    
+    return fib[1];
+}
 
+int main() {
+    int n;
+    
+    printf("Ingrese el valor de n: ");
+    scanf("%d", &n);
+    int resultado = fibonacci(n);
+    
+    printf("El término Fibonacci número %d es: %d\n", n, resultado);
+    
+    return 0;
+}
